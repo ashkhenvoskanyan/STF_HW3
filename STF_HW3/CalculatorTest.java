e139530ce6a1552f7d36e39852842b8c334556df
ashkhenvoskanyan <43004388+ashkhenvoskanyan@users.noreply.github.com>
2021-04-13 22:25:23 +0400

    Add files via upload

---
+++ STF_HW3/CalculatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void main() {
+
+    }
+
+    @Test
+    void addition() {
+    }
+
+    @Test
+    void subtraction() {
+    }
+
+    @Test
+    void division() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+}
